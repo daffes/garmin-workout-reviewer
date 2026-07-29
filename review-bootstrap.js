0fc822e6a74be1ff974dc7b8d05ca95aebe79f3a
daffes <daffes@gmail.com>
2026-07-29 18:14:38 -0300

    Load Garmin link manifest capture

--- review-bootstrap.js
+++ review-bootstrap.js
@@ -1,5 +1,6 @@
 import "./activity-index.js?v=20260729-1801";
+import "./manifest-capture.js?v=20260729-1814";
 import "./compact-review.js?v=20260729-1749";
 import "./storage-delete.js?v=20260729-1801";
 await import("./app.js?v=20260729-1701");
-await import("./library-columns.js?v=20260729-1731");
+await import("./library-columns.js?v=20260729-1814");
