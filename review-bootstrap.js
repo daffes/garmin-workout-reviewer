38b8dc41ea5aa0a77e71c49ed02003d18b4f60b1
daffes <daffes@gmail.com>
2026-07-29 18:20:40 -0300

    Use activity-specific review prompt

--- review-bootstrap.js
+++ review-bootstrap.js
@@ -1,5 +1,6 @@
 import "./activity-index.js?v=20260729-1801";
 import "./manifest-capture.js?v=20260729-1814";
+import "./activity-review-prompt.js?v=20260729-1822";
 import "./compact-review.js?v=20260729-1749";
 import "./storage-delete.js?v=20260729-1801";
 await import("./app.js?v=20260729-1701");
