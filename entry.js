2286ff24d789b6c2c27ef0332a03f84248e5a211
daffes <daffes@gmail.com>
2026-07-29 17:59:15 -0300

    Load storage and delete enhancements

---
+++ entry.js
@@ -0,0 +1,4 @@
+import "./compact-review.js?v=20260729-1749";
+import "./storage-delete.js?v=20260729-1756";
+await import("./app.js?v=20260729-1701");
+await import("./library-columns.js?v=20260729-1731");
