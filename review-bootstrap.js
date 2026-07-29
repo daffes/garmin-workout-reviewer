697217d1727e7e1e85ee1adab71480662a09a1e0
daffes <daffes@gmail.com>
2026-07-29 18:07:10 -0300

    Enable indexed activity library

--- review-bootstrap.js
+++ review-bootstrap.js
@@ -1,3 +1,5 @@
+import "./activity-index.js?v=20260729-1801";
 import "./compact-review.js?v=20260729-1749";
+import "./storage-delete.js?v=20260729-1801";
 await import("./app.js?v=20260729-1701");
 await import("./library-columns.js?v=20260729-1731");
